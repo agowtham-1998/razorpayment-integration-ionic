dff95d50c265e870c1b624e29ae944a8c36e9e99
agowtham <gowthamadesk@gmail.com>
2022-02-04 14:17:28 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # razorpayment-integration-ionic
+
+# cordova plugin add com.razorpay.cordova --save
+
+# ionic cordova platform add browser
+
+# ionic cordova run browser
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var RazorpayCheckout: any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  paymentAmount: number = 333;
+  currency: string = 'USD';
+  currencyIcon: string = '$';
+  razor_key = 'rzp_test_urJcWW5CCTcr8V';
+  cardDetails: any = {};
+
+  constructor() {
+  }
+
+  payWithRazor() {
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: this.currency,
+      key: this.razor_key,
+      amount: this.paymentAmount,
+      name: 'foo',
+      prefill: {
+        email: 'admin@enappd.com',
+        contact: '9621323231',
+        name: 'Enappd'
+      },
+      theme: {
+        color: '#F37254'
+      },
+      modal: {
+        ondismiss: function () {
+          alert('dismissed')
+        }
+      }
+    };
+
+    var successCallback = function (payment_id) {
+      alert('payment_id: ' + payment_id);
+    };
+
+    var cancelCallback = function (error) {
+      alert(error.description + ' (Error ' + error.code + ')');
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
+
 
 }
